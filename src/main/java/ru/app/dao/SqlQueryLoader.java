2b3c23839fa670df41cc69c4d1ab961eff5f98ea
Wrls <not_set>
2025-11-12 00:27:21 +0700

    unmodifiableMap for queries

--- src/main/java/ru/app/dao/SqlQueryLoader.java
+++ src/main/java/ru/app/dao/SqlQueryLoader.java
@@ -1,20 +1,16 @@
 package ru.app.dao;
 
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @Component
@@ -35,26 +31,25 @@ public class SqlQueryLoader {
                 + SQL_RESOURCES + (path.startsWith("/") ? path : "/" + path));
     }
 
-    private final Map<String, String> QUERIES = new ConcurrentHashMap<>();
+    private final Map<String, String> QUERIES;
 
     private static final String SQL_RESOURCES = "src/main/resources/sql";
 
-    @PostConstruct
-    private void loadAllSqlFiles() throws IOException {
-        QUERIES.clear();
+    private SqlQueryLoader() throws IOException {
+        Map<String, String> loadMap = new HashMap<>();
         Path basePath = Paths.get(SQL_RESOURCES);
 
         if (!Files.exists(basePath)) {
             throw new FileNotFoundException("Directory not found: " + SQL_RESOURCES);
         }
-
+        log.info("============= SqlQueryLoader scan resources start =============");
         Files.walkFileTree(basePath, new SimpleFileVisitor<Path>() {
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                 if (file.toString().endsWith(".sql")) {
                     String content = Files.readString(file);
                     String path = basePath.relativize(file).toString();
-                    QUERIES.put(path, content);
+                    loadMap.put(path, content);
                     log.info("Add query from path: {}", path);
                 }
                 return FileVisitResult.CONTINUE;
@@ -66,6 +61,7 @@ public class SqlQueryLoader {
                 return FileVisitResult.CONTINUE;
             }
         });
-
+        log.info("============= SqlQueryLoader scan resources finish, find: {} scripts =============", loadMap.size());
+        QUERIES = Collections.unmodifiableMap(loadMap);
     }
 }
